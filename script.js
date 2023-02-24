90dae1a516c08c71a22e03a034acdf9068438c5f
ppyrzyk <115031262+ppyrzyk@users.noreply.github.com>
2023-02-24 17:11:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/tasks';
+  const apiRoot = 'https://kodilla-TaskApplication.ppyrzyk.repl.co';
   const trelloApiRoot = 'http://localhost:8080/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
